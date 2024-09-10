5ed2befd309c5998a82c7c33e0f320ccf7b154fc
LeonardoF22 <leonardoferodrigues7@gmail.com>
2024-09-10 12:12:27 -0300

    css

--- simulador/calcular.php
+++ simulador/calcular.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simulador de emprestimo</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
 </head>
 <body>
-    <h2>Seja bem vindo ao MyBank</h2>
-    <h3>Resultado da Simulação</h3>
-
 <?php
     $cliente = $_POST['cliente']; // 's' para sim ou 'n' para não
     $score = $_POST['score']; // Score do serasa
@@ -41,9 +40,15 @@
     $valor_total = $valor + $valor * 0.038;
     $valorParcela = $valor_total / $parcelas;
     ?>
-    <p>Valor das parcelas: R$<?= number_format($valorParcela, 2, ",", ".")?></p>
-    <p>Quantidade de parcelas: <?= $parcelas?></p>
-    <p>Taxa de juros: <?= $juros*100 ?>%</p>
-    <p>Valor total CET: R$<?= number_format($valor_total, 2, ",", ".") ?></p>
+    <div class="container">
+        <h2>Seja bem-vindo ao MyBank</h2>
+        <div class="resultado">
+            <h3>RESULTADO DA SIMULAÇÃO</h3>
+            <p>Valor das parcelas: R$<?= number_format($valorParcela, 2, ",", ".")?></p>
+            <p>Quantidade de parcelas: <?= $parcelas?></p>
+            <p>Taxa de juros: <?= $juros*100 ?>%</p>
+            <p>Valor total CET: R$<?= number_format($valor_total, 2, ",", ".") ?></p>
+        </div>
+    </div>
 </body>
 </html>
